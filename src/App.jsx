bc2a270fa959f380792201365eea0af1de881373
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2023-10-03 22:03:31 -0500

    Course selection

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,31 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import CourseList from './components/CourseList';
-
-
-
+import CourseList from './components/CourseList.jsx';
+import TermSelector from './components/TermSelector.jsx';
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <div className="container">
-      <CourseList />
-    </div>
-  </QueryClientProvider>
-);
+const App = () => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
+
+  const toggleSelected = (item) => setSelected(
+    selected.includes(item)
+    ? selected.filter(x => x !== item)
+    : [...selected, item]
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="container">
+        <h1>CS Courses for 2018-2019</h1>
+        <TermSelector selection={term} setSelection={setTerm} />
+        <CourseList term={term} selected={selected} toggleSelected={toggleSelected} />
+
+      </div>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
 import { useJsonQuery } from '../utilities/fetch';
-import TermSelector from './TermSelector';
+import TermSelector from './TermSelector.jsx';
 
-const CourseList = () => {
-    const [term, setTerm] = useState('Fall');
+const CourseList = ({ term, selected = [], toggleSelected }) => {
     const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
   
     if (error) return <h1>Error loading courses data: {`${error}`}</h1>;
@@ -11,22 +10,23 @@ const CourseList = () => {
     if (!data) return <h1>No courses data found</h1>;
   
     return (
-      <>
-      <h1>CS Courses for 2018-2019</h1>
-      <TermSelector selection={term} setSelection={setTerm} />
       <div className="course-grid">
         {Object.entries(data.courses)
         .filter(([key, course]) => course.term === term)
         .map(([key, course]) => (
-          <div className="card" key={key}>
+          <div 
+            className={`card ${selected.includes(key) ? "selected" : ""}`}
+            key={key} 
+            onClick={() => toggleSelected && toggleSelected(key)}
+          >
             <div className="card-title">{course.term} CS {course.number}</div>
             <div className="card-body">{course.title}</div>
             <div className="card-footer">{course.meets}</div>
           </div>
         ))}
       </div>
-      </>
     );
-  };
+};
+
 
 export default CourseList;
