408299bcefe7a1999c94879dc5b3d2364eb05440
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2023-10-08 09:18:30 -0500

    fix catch time conflicts

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,33 +1,20 @@
 import React, { useState } from 'react';
 import '../App.css';
+import './CourseCard.css';
 import { isConflicting } from '../utilities/CheckConflict.js'; 
 
 const CourseCard = ({ course, isSelected, isConflicting, onToggle }) => {
-    const [showConflictWarning, setShowConflictWarning] = useState(false);
-    const handleCourseClick = () => {
-        if (isConflicting && isSelected) {
-            setShowConflictWarning(true);
-        } else {
-            onToggle(course.number, course);
-            setShowConflictWarning(false); 
-        }
-    };
-    const cardStyle = {
-        borderColor: isConflicting ? 'red' : 'defaultBorderColor', 
-    };
-    
     return (
         <div 
-            className={`card ${isSelected ? "selected" : ""}`} style={cardStyle}
-            onClick={handleCourseClick}
+            className={`card ${isSelected ? "selected" : ""}`}
+            onClick={() => onToggle(course.number, course)}
         >
+            {isConflicting && <span className="warning-icon">⚠️</span>}
             <div className="card-title">{course.term} CS {course.number}</div>
             <div className="card-body">{course.title}</div>
             <div className="card-footer">{course.meets}</div>
-            {showConflictWarning && <p style={{ color: 'red' }}>It is conflicted</p>}
         </div>
     );
 };
-
 
 export default CourseCard;
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -2,25 +2,29 @@ import CourseCard from './CourseCard.jsx';
 import { getConflictingCourses } from '../utilities/CheckConflict.js';
 
 const CourseList = ({ term, selected = [], toggleSelected, courses = {} }) => {
-  const selectedCourses = selected.map(key => courses[key]);
    return(
       <div className="course-grid">
         {Object.entries(courses)
         .filter(([key, course]) => course.term === term)
         .map(([key, course]) => {
+          const selectedCourses = selected.map(selKey => courses[selKey]);
           const conflictingCourses = getConflictingCourses(course, selectedCourses);
           const isConflicting = conflictingCourses.length > 0;
+
           return(
             <CourseCard 
             key={key}
             course={course}
             isSelected={selected.includes(key)}
-            isConflicting={isConflicting}
+            isConflicting={isConflicting && !selected.includes(key)}
             onToggle={() => {
               if (!isConflicting || selected.includes(key)) {
                 toggleSelected && toggleSelected(key);
-            }}}
+              }
+            }}
           />
+          
+          
         )})}
         
       </div>
