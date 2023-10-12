ac33000c1d3364165c92cb4e5b4c9a4477f5151f
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2023-10-11 20:19:23 -0500

    Add form validation

--- src/components/CourseForm.jsx
+++ src/components/CourseForm.jsx
@@ -3,7 +3,16 @@ import { useFormData } from '../utilities/useFormData';
 
 const CourseForm = ({ course, onCancel }) => {
   const validateCourseData = (key, val) => {
-    return '';
+    switch (key) {
+      case 'title':
+        return val.length >= 2 ? '' : 'Title must be at least two characters';
+      case 'meets':
+        // Validating empty string or format like "MWF 12:00-13:20"
+        const meetsPattern = /^$|^[MTWThF]+ \d{2}:\d{2}-\d{2}:\d{2}$/;
+        return meetsPattern.test(val) ? '' : 'Must contain days and start-end, e.g., MWF 12:00-13:20';
+      default:
+        return '';
+    }
   };
 
   const [state, change] = useFormData(validateCourseData, course);
@@ -12,20 +21,27 @@ const CourseForm = ({ course, onCancel }) => {
     evt.preventDefault();
   };
   
-  const InputField = ({name, text, state, change}) => (
-    <div className="mb-3">
-      <label htmlFor={name} className="form-label">{text}</label>
-      <input className="form-control" id={name} name={name} 
-        defaultValue={state.values?.[name]} onChange={change} />
+  const InputField = ({ name, text, state, change }) => (
+    <div className="form-group">
+      <label htmlFor={name}>{text}</label>
+      <input
+        type="text"
+        className={`form-control ${state.errors?.[name] ? 'is-invalid' : ''}`}
+        id={name}
+        name={name}
+        value={state.values[name] || ''}
+        onChange={change}
+      />
       <div className="invalid-feedback">{state.errors?.[name]}</div>
     </div>
   );
+  
 
   return (
     <form onSubmit={handleSubmit}>
       <InputField name="title" text="Course Title" state={state} change={change} />
       <InputField name="meets" text="Meeting Times" state={state} change={change} />
-      <button type="button" onClick={onCancel}>Cancel</button>
+      <button type="button" onClick={onCancel} className="btn btn-secondary">Cancel</button>
     </form>
   );
 };
